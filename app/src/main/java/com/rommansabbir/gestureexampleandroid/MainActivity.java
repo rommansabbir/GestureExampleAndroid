bf9e7ce25e7374b97a87847640b4e581d379e68c
rommansabbir <misterxero96@gmail.com>
2019-02-05 08:37:35 +0600

    added: video player with local video

--- app/src/main/java/com/rommansabbir/gestureexampleandroid/MainActivity.java
+++ app/src/main/java/com/rommansabbir/gestureexampleandroid/MainActivity.java
@@ -2,17 +2,29 @@ package com.rommansabbir.gestureexampleandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.github.rtoshiro.view.video.FullscreenVideoLayout;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewGesture;
     private View fullScreenLayout;
     private View playlistLayout;
+    private FullscreenVideoLayout videoLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +33,22 @@ public class MainActivity extends AppCompatActivity {
         playlistLayout = findViewById(R.id.playlistLayout);
         playlistLayout.setVisibility(View.GONE);
 
-        textViewGesture = findViewById(R.id.textViewGesture);
+
+        videoLayout = (FullscreenVideoLayout) findViewById(R.id.videoview);
+        videoLayout.setActivity(this);
+
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback);
+        try {
+            videoLayout.setVideoURI(videoUri);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        textViewGesture = findViewById(R.id.textViewGesture);
         fullScreenLayout.setOnTouchListener(new OnSwipeTouchListner(this){
             public void onSwipeTop() {
-                textViewGesture.setText("Swipe Top");
+//                textViewGesture.setText("Swipe Top");
                 if(playlistLayout.getVisibility() == View.GONE){
 
                     swipeUpAnimation(playlistLayout.getHeight(), 0);
@@ -37,15 +61,15 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onSwipeRight() {
-                textViewGesture.setText("Swipe Right");
+//                textViewGesture.setText("Swipe Right");
             }
 
             public void onSwipeLeft() {
-                textViewGesture.setText("Swipe Left");
+//                textViewGesture.setText("Swipe Left");
             }
 
             public void onSwipeBottom() {
-                textViewGesture.setText("Swipe Bottom");
+//                textViewGesture.setText("Swipe Bottom");
                 if(playlistLayout.getVisibility() == View.VISIBLE){
 
                     swipeBottomAnimation(0, playlistLayout.getHeight());
@@ -59,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private void swipeBottomAnimation(int i, int height) {
         TranslateAnimation animate = new TranslateAnimation(
